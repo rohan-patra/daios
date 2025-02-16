36aa2380f4222c24c7dfe51bc7d4d9f93d520f8f
Vardhan Shorewala <vardhanshorewala@Vardhans-MacBook-Air.local>
2025-02-15 17:44:33 -0800

    Added backend structure for python

--- src/env.js
+++ src/env.js
@@ -11,8 +11,6 @@ export const env = createEnv({
       process.env.NODE_ENV === "production"
         ? z.string()
         : z.string().optional(),
-    AUTH_DISCORD_ID: z.string(),
-    AUTH_DISCORD_SECRET: z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
